a22dfb5dbd430e249ce11533e5c5559e3d9b7d7d
Doe Mohamud <doe.mahamud1@gmail.com>
2021-07-05 17:51:52 -0400

    commit tree 40455ca268531461328a4157635d4f5fc33b33b9

--- app.rb
+++ app.rb
@@ -3,4 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do 
+     @user_name = params[:name]
+     "#{@user_name.reverse}"
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number ** 2}"
+  end
+
 end
